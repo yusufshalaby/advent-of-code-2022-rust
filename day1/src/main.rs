f965e0798f54ca2a393cdda76cf3ccdd41fc1cc4
Yusuf Shalaby <yusufshalaby@FT-MAC-YusufS.local>
2023-09-19 15:10:10 -0400

    day 1 generalized

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,29 +1,11 @@
 use std::{collections::VecDeque, num::ParseIntError};
 
-fn part1(input: &String) -> Result<u32, ParseIntError> {
-    let mut elf_max_calories: u32 = 0;
+fn get_top_k_most_calories(input: &String, k: &usize) -> Result<u32, ParseIntError> {
+    let mut deque = VecDeque::from(vec![0; *k]);
     let mut elf_total_calories: u32 = 0;
     for line in input.lines() {
         if line == "" {
-            elf_total_calories = 0;
-            continue;
-        }
-        let calories: u32 = line.parse()?;
-        elf_total_calories += calories;
-        if elf_total_calories > elf_max_calories {
-            elf_max_calories = elf_total_calories;
-        }
-    }
-
-    Ok(elf_max_calories)
-}
-
-fn part2(input: &String) -> Result<u32, ParseIntError> {
-    let mut top3 = VecDeque::from([0, 0, 0]);
-    let mut elf_total_calories: u32 = 0;
-    for line in input.lines() {
-        if line == "" {
-            update_top3(&mut top3, &elf_total_calories);
+            update_deque(&mut deque, &elf_total_calories);
             elf_total_calories = 0;
             continue;
         }
@@ -32,33 +14,33 @@ fn part2(input: &String) -> Result<u32, ParseIntError> {
         elf_total_calories += calories;
     }
 
-    let top3_total = top3.iter().sum();
-    Ok(top3_total)
+    let deque_total = deque.iter().sum();
+    Ok(deque_total)
 }
 
-fn update_top3(top3: &mut VecDeque<u32>, elf_total_calories: &u32) -> () {
+fn update_deque(deque: &mut VecDeque<u32>, elf_total_calories: &u32) -> () {
     let mut i: usize = 0;
-    for val in top3.iter() {
+    for val in deque.iter() {
         if elf_total_calories < val {
             break;
         }
         i += 1;
     }
     if i > 0 {
-        top3.insert(i, *elf_total_calories);
-        top3.pop_front();
+        deque.insert(i, *elf_total_calories);
+        deque.pop_front();
     }
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("unable to read input.txt");
-    let part1result = part1(&input);
+    let part1result = get_top_k_most_calories(&input, &1);
     match part1result {
         Ok(answer) => println!("The answer to part 1 is {answer}"),
         Err(e) => println!("Error in part 1: {e}"),
     }
 
-    let part2result = part2(&input);
+    let part2result = get_top_k_most_calories(&input, &3);
     match part2result {
         Ok(answer) => println!("The answer to part 2 is {answer}"),
         Err(e) => println!("Error in part 2: {e}"),
