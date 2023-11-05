7d25629a73d368faef759c13d0f130318cf39c0b
yusufshalaby <yusufshalaby@gmail.com>
2023-11-05 00:00:50 -0400

    some cleanup in 12b

--- day12b/src/main.rs
+++ day12b/src/main.rs
@@ -20,20 +20,13 @@ struct State {
     cost: usize,
     position: usize,
 }
-// The priority queue depends on `Ord`.
-// Explicitly implement the trait so the queue becomes a min-heap
-// instead of a max-heap.
+
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        // Notice that the we flip the ordering on costs.
-        // In case of a tie we compare positions - this step is necessary
-        // to make implementations of `PartialEq` and `Ord` consistent.
         other.cost.cmp(&self.cost)
-        // .then_with(|| self.position.cmp(&other.position))
     }
 }
 
-// `PartialOrd` needs to be implemented as well.
 impl PartialOrd for State {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
